e4dde20890f041c8b11349c646a0cbd97491e7dc
player-geun <player_geun@naver.com>
2022-07-23 19:48:51 +0900

    1008 - a/b

---
+++ src/baekjoon/q1008/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.q1008;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextDouble() / sc.nextDouble());
+    }
+}
