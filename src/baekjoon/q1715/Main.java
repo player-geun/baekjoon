509cb6f48d5ee67b43b133586bc7b4a00212c22b
player-geun <player_geun@naver.com>
2022-07-24 12:36:17 +0900

    1715 - 카드 정렬하기
    우선순위큐
    그리디

---
+++ src/baekjoon/q1715/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.q1715;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            pq.add(sc.nextInt());
+        }
+
+        int data1 = 0;
+        int data2 = 0;
+        int sum = 0;
+
+        while (pq.size() != 1) {
+            data1 = pq.remove();
+            data2 = pq.remove();
+            sum += data1 + data2;
+            pq.add(data1 + data2);
+        }
+
+        System.out.println(sum);
+    }
+}
