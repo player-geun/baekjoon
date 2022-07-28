9a9bf32cd85dde254fa82f834de3c3fa056d7fa1
player-geun <player_geun@naver.com>
2022-07-28 15:48:00 +0900

    11689 - cgd
    오일러 피

---
+++ src/baekjoon/q11689/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.q11689;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long result = n;
+
+        for (long i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                result -= result / i;
+
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+
+        if (n > 1) {
+            result -= result / n;
+        }
+
+        System.out.println(result);
+    }
+}
