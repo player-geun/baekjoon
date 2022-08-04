626a878226890fac1bbe56986cfb204f4f23ee3f
player-geun <player_geun@naver.com>
2022-08-04 21:26:41 +0900

    2742 - N 거꾸로

---
+++ src/baekjoon/q2742/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.q2742;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
