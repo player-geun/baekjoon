3a24f45588a7058a61aad7ec0b7896e548987dd7
player-geun <player_geun@naver.com>
2022-08-22 15:44:49 +0900

    11654

---
+++ src/baekjoon/q11654/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.q11654;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.next().charAt(0);
+
+        System.out.println(A);
+    }
+}
