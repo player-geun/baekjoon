f9d263a3142e5e2bd332c4a8773d7ec0912481f3
player-geun <player_geun@naver.com>
2023-06-19 09:56:45 +0900

    백준 문제

--- src/baekjoon/q1520/Main.java
+++ src/baekjoon/q1520/Main.java
@@ -1,8 +1,56 @@
 package baekjoon.q1520;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+        Deque<Integer> deque = new LinkedList<>();
+        deque.add(1);
+        deque.add(2);
+        deque.add(3);
+
+        deque.addLast(deque.pop());
+        System.out.println();
+        Set<Integer> set = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>(new int[]{});
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> resultList = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+        while (start < progresses.length) {
+            for (int i = start; i < progresses.length; i++) {
+                progresses[i] += speeds[i];
+            }
+
+            for (int i = start; i < progresses.length; i++) {
+                if (progresses[i] < 100) {
+                    if (i != start && progresses[i - 1] == 100) {
+                        resultList.add(count);
+                        count = 0;
+                        start = i;
+                    }
+                    break;
+                }
+                count++;
+            }
+        }
+
+        return resultList.stream().mapToInt(e -> e).toArray();
+    }
+
+    private static List<Integer> toList(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        return list;
     }
 }
